f04995b9f6eab11e08208fe393cbebd848af01f1
Tommy Thach <thachtommy5@gmail.com>
2024-06-23 11:58:48 +0700

    add code

--- src/app/questions/result/page.js
+++ src/app/questions/result/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { Suspense } from "react";
 import { useQuiz } from "@/app/context/QuizContext";
 import Result from "@/components/Result";
 import { useRouter, useSearchParams } from "next/navigation";
@@ -17,14 +18,16 @@ export default function ResultPage() {
     }
   }
   return (
-    <div className="container mx-auto p-4 text-white flex gap-7 flex-col mt-10">
-      <h1 className="text-sm font-semibold text-gray-400 uppercase mb-4 text-center">
-        ĐÁNH GIÁ MỨC ĐỘ TRƯỞNG THÀNH <br />
-        VỀ QUẢN TRỊ TRẢI NGHIỆM KHÁCH HÀNG
-      </h1>
-      <div className="rounded-md p-4 w-full bg-[#395273]">
-        <Result level={level} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="container mx-auto p-4 text-white flex gap-7 flex-col mt-10">
+        <h1 className="text-sm font-semibold text-gray-400 uppercase mb-4 text-center">
+          ĐÁNH GIÁ MỨC ĐỘ TRƯỞNG THÀNH <br />
+          VỀ QUẢN TRỊ TRẢI NGHIỆM KHÁCH HÀNG
+        </h1>
+        <div className="rounded-md p-4 w-full bg-[#395273]">
+          <Result level={level} />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
